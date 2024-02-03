351b0db73c898968e4480f9bf11b1fefcc33d47d
atremat <atremat@gmail.com>
2024-02-03 10:42:24 +0000

    Deploying to gh-pages from @ NikOvchinnik/team-vite-tasks@00bf41564db62cd8c6ee6983ea0e1c9f0f7fcaf7 🚀

---
+++ assets/vendor-342e672c.js
@@ -0,0 +1,2 @@
+const l="useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";let r=(t=21)=>{let e="",n=crypto.getRandomValues(new Uint8Array(t));for(;t--;)e+=l[n[t]&63];return e};export{r as n};
+//# sourceMappingURL=vendor-342e672c.js.map
