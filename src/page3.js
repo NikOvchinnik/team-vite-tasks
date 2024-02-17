9b09c7123d0f3d3f464c19ef1f5662ecba8b7ca3
NikOvchinnik <n.ovchinnik.s@gmail.com>
2024-02-17 12:39:51 +0200

    final

--- src/page3.js
+++ src/page3.js
@@ -11,3 +11,21 @@ async function getContactsById(id) {
   const data = await axios.get(`/contacts/${id}`);
   console.log(data.data);
 }
+
+async function postContacts(nameUser, phoneNumber) {
+  const data = await axios.post('/contacts', {
+    name: nameUser,
+    phone: phoneNumber,
+  });
+}
+
+async function putContacts(id, nameUser, phoneNumber) {
+  const data = await axios.put(`/contacts/${id}`, {
+    name: nameUser,
+    phone: phoneNumber,
+  });
+}
+
+async function deleteContacts(id) {
+  const data = await axios.delete(`/contacts/${id}`);
+}
