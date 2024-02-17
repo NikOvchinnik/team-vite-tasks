e392cfc202a932a0b079e9b18b4e6be522fe8e69
NikOvchinnik <136114400+NikOvchinnik@users.noreply.github.com>
2024-02-17 09:04:52 +0000

    Deploying to gh-pages from @ NikOvchinnik/team-vite-tasks@d6d834eb9830bc07faeda75d57468f253a874648 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import{g as a,a as o}from"./assets/localValue-3aff6171.js";const t=document.querySelector(".task2-list");console.log(a("task"));o(t,"task");
+//# sourceMappingURL=commonHelpers2.js.map
